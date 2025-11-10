54c73c431cbe3061c77b12ec2cd1a2fb174be86c
Antonia D. <admariscal1@my.scccd.edu>
2025-11-10 22:06:19 +0000

    Last practice for the day

--- Mariscal14_3.m
+++ Mariscal14_3.m
@@ -1,1 +1,23 @@
+% Special Functions for Polynomials
+% Integral and derivative of a polynomial 
+p = input("Enter the coefficients of a cubic polynomial," + ...
+    " as a matrix,[...]\n");
+y0 = input("Enter the integration constant for that function.\n");
+pDer = polyder(p);
+pint = polyint(p,y0);
+% Report the Output 
+fprintf("The derivative of the function is %gx^2+%gx+%g.\n",pDer)
+fprintf("The intergral of the function is %gx^4+%gx^3+%gx^2+%gx+%g.\n",pint)
+
+
+
+
+
+
+
+
+
+
+
+
 
